e8cad2b4b010113ce5141b25ac1c1aafff542cfc
Xephyrka <edfollow.omar@gmail.com>
2026-09-23 11:26:58 +0900

    hw1

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    printf("Sum of two numbers: %d\n", x + y);
+    printf("Difference of two numbers: %d\n", x - y);
+    printf("Product of two numbers: %d\n", x * y);
+    printf("Quotient of two numbers: %d\n", x / y);
+    printf("Remainder of two numbers: %d\n", x % y);
+
+    // int a;
+    // float b;
+    // char c;
+    // scanf("%d %f %c", &a, &b, &c);
+    // printf("%d %f %c", a, b, c);
+    // return 0;
 }
